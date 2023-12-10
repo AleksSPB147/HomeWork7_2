cb7d763842ca772ad446f1a845956f09a2404fbc
AleksSPB147 <146634332+AleksSPB147@users.noreply.github.com>
2023-12-10 20:29:33 +0300

    Сделано

--- HomeWork7_2.cpp
+++ HomeWork7_2.cpp
@@ -1,23 +1,29 @@
 ﻿#include <iostream>
-#include<stdio.h>
+//#include<stdio.h>
 using namespace std;
 //int F(int n);
 //int G(int n);
-
+//Напишите программу для  вычисления значения функции F(n), где n — натуральное число, задана следующими соотношениями :
+//F(1) = 1;
+//F(n) = n + F(n − 1), если n чётно;
+//F(n) = 2 · F(n − 2), если n > 1 и при этом нечётно.
+//Пользователь вводит n, программа должна вывести значение F(n).
 int F(int n)
 {
     if (n == 1)
-    
         return 1;
-
-        if (n % 2 == 0)
-        return  n + F(n - 1);
-       
-        else
-
-        return  2 * F(n - 2);
-
+    if (n %2== 0)
+    return  n + F(n - 1);
+    else
+    return  2 * F(n - 2);
     
+    
+    
+    
+  
+
+        
+
     
 
 
@@ -37,7 +43,7 @@ int main()
     cin >> n;
     F(n);
     cout << endl;
-    cout << F(n);
+    cout<< "Значение F(n) = " << F(n);
     //result = F(n) + G(m);
     //cout << "F(n) = " << F(n) << ";" << endl;
     //cout << "G(n) = " << G(n) << ";" << endl;
